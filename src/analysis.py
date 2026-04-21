d57568db1d480faa112321f9bd9a1c958dc25378
Dianette Jimenez <dianettejimenez@gmail.com>
2026-04-21 19:46:05 +0000

    need to adjust batch size, but otherwise have part 2 done

--- src/analysis.py
+++ src/analysis.py
@@ -18,8 +18,7 @@ task = info['task']
 n_channels = info['n_channels']
 n_classes = 7
 
-NUM_EPOCHS = 3
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 lr = 0.001
 
 DataClass = getattr(medmnist, info['python_class'])
@@ -52,6 +51,10 @@ val_tensor_flat = val_tensor.flatten(start_dim = 1)
 train_tensor_flat = train_tensor.flatten(start_dim = 1)
 test_tensor_flat = test_tensor.flatten(start_dim = 1)
 
+validation_dataset_transformed = DermaMNIST(split="val", download=True, transform=data_transform)
+training_dataset_transformed = DermaMNIST(split="train", download=True, transform=data_transform)
+testing_dataset_transformed = DermaMNIST(split="test", download=True, transform=data_transform)
+
 class MulticlassLogisticRegression(nn.Module):
     def __init__(self, input_size, num_classes):
         super(MulticlassLogisticRegression, self).__init__()
@@ -60,20 +63,22 @@ class MulticlassLogisticRegression(nn.Module):
         out = self.linear(x)
         return out
 
-model = MulticlassLogisticRegression(BATCH_SIZE, n_classes)
+model = MulticlassLogisticRegression(2352, n_classes)
 
-dataloader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
+dataloader = DataLoader(training_dataset_transformed, batch_size=BATCH_SIZE, shuffle=True)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 # Training loop
-num_epochs = 100
+num_epochs = 300
 for epoch in range(num_epochs):
-    for inputs in val_tensor_flat:
+    for inputs, labels in dataloader:
+        # Flatten inputs for linear model
+        inputs = inputs.flatten(start_dim=1)
         # Forward pass
         outputs = model(inputs)
-        loss = criterion(outputs)
+        loss = criterion(outputs, labels.squeeze())
  
         # Backward and optimize
         optimizer.zero_grad()
